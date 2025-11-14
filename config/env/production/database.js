d93d217d97330c5231a7029afa77f532ae3065ca
Carlos Osorio <hacybeyker@gmail.com>
2025-11-14 11:09:56 -0500

    added database.js and pg

---
+++ config/env/production/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', false),
+
+    },
+    debug: false,
+    // Opcional pero recomendado para producción
+    pool: {
+      min: 0,
+      max: 15
+    }
+  },
+});
